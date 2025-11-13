febc3c9eb0cf1b7d0bd1347723af7c2c0681db32
CarrieForle <53133715+CarrieForle@users.noreply.github.com>
2025-11-13 16:54:46 +0800

    fix help message

--- src/command_line.rs
+++ src/command_line.rs
@@ -40,5 +40,5 @@ A resource is a URL linked to the things you want to download and optionally a b
 
 In "{file_name}", each resource is separated by a newline. Each line contains a URL and optionally some options. The URL and each option are separated by a whitespace.
 In a pixiv resource, the URL should looks like "https:///www.pixiv.net/artworks/<illust_id>". If there are multiple artworks for a given URL, you can optionally specify either a range (<start>..<end>) or any number of index of illustration to only download some of the files. The index starts from 1 and the range are inclusive. Not specifying any will download all artworks.c
-For example: "https:///www.pixiv.net/artworks/1234 1..2" will download the first and second illustration.;"#)
+For example: "https:///www.pixiv.net/artworks/1234 1..2" will download the first and second illustration."#)
 }
